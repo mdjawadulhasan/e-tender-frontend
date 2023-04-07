b319c8d269dc748223a34dcd3468230afd4d3f26
jawadbs23 <66155771+mdjawadulhasan@users.noreply.github.com>
2023-04-07 18:00:28 +0600

    Logo Added

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,9 +1,15 @@
-import React from "react";
+import Link from "next/link"
+import MyLayout from "@/pages/admin/component/layout"
+export default function AdminDashboard() {
 
-export default function index() {
-  return <div>
-    <h1>
-    This is Tender Manager Home Page
-    </h1>
-  </div>;
-}
+    return (
+      <>
+      <MyLayout title="Admin DashBoard"/>
+      <h1>Admin Dashboard</h1>
+
+    <Link href="/admin/dashboard/getusers">See All Users</Link>
+    <br></br>
+    <Link href="/admin/dashboard/findusers">Find Users by ID</Link>
+      </>
+    )
+  }
