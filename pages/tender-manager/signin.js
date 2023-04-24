211d2bb2b1a68b5ff80506501431fef41f208675
jawadbs23 <66155771+mdjawadulhasan@users.noreply.github.com>
2023-04-24 15:46:49 +0600

    SignUp Page Done

--- pages/tender-manager/signin.js
+++ pages/tender-manager/signin.js
@@ -40,27 +40,38 @@ export default function SignIn() {
         <h2 className="text-2xl font-bold">Welcome To E-Tender</h2>
       </div>
 
-      
+
       <form onSubmit={handleSubmit} className="max-w-sm mx-auto mt-12">
         <div className="mb-4">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
             Email
           </label>
-          <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
         </div>
         <div className="mb-4">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
             Password
           </label>
-          <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
         </div>
         <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          <button className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
             Sign In
           </button>
         </div>
+
         {error && <p className="text-red-500 mt-4">{error}</p>}
       </form>
+
+      <div className="flex justify-center items-center mt-12">
+        <button
+          type="button"
+          onClick={() => router.push('/tender-manager/signup')}
+          className="mt-4 block underline hover:text-blue-500"
+        >
+          Not Reigterd yet ? Click here to Sign Up
+        </button>
+      </div>
     </>
   )
 }
--- pages/tender-manager/signup.js
+++ pages/tender-manager/signup.js
@@ -3,6 +3,7 @@ import MyLayout from "@/pages/tender-manager/component/layout";
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form';
 import axios from "axios"
+import Head from 'next/head';
 
 export default function SignUp() {
   const router = useRouter();
@@ -20,7 +21,7 @@ export default function SignUp() {
       console.log(file.type);
       return false;
     }
-  
+
     return true;
   }
 
@@ -49,7 +50,7 @@ export default function SignUp() {
 
     }
     catch (error) {
-      
+
       setSuccess('Signup unsuccessfull ' + error.response.data.message);
 
     }
@@ -60,29 +61,37 @@ export default function SignUp() {
   return (
     <>
       {/* <SessionCheck /> */}
-      <MyLayout title="Signup" />
-      <h1>Signup</h1>
+
+      <Head>
+        <title>Sign In</title>
+        <link rel="icon" type="image/x-icon" href="/logo.png"></link>
+      </Head>
+      <div className="flex justify-center items-center mt-12">
+        <img src="/logo.png" alt="Logo" className="w-16 h-16 mr-2" />
+        <h2 className="text-2xl font-bold">Welcome To E-Tender</h2>
+      </div>
       {success}
-      <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
+      <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data" className="max-w-sm mx-auto mt-1">
         <div>
-          <label htmlFor="name">Name</label>
+          <label htmlFor="name" className="block font-medium">Name</label>
           <input
             type="text"
             id="name"
-
+            className="w-full border border-gray-400 p-2 rounded-md"
             {...register('name', { required: true })}
           />
-          {errors.name && <p>Name is required</p>}
+          {errors.name && <p className="text-red-500">Name is required</p>}
         </div>
         <div>
-          <label htmlFor="email">Email</label>
+          <label htmlFor="email" className="block font-medium">Email</label>
           <input
             type="email"
             id="email"
+            className="w-full border border-gray-400 p-2 rounded-md"
             {...register('email', { required: true, pattern: /\S+@\S+\.\S+/ })}
           />
           {errors.email && (
-            <p>
+            <p className="text-red-500">
               {errors.email.type === 'required'
                 ? 'Email is required'
                 : 'Invalid email address'}
@@ -90,14 +99,15 @@ export default function SignUp() {
           )}
         </div>
         <div>
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password" className="block font-medium">Password</label>
           <input
             type="password"
             id="password"
+            className="w-full border border-gray-400 p-2 rounded-md"
             {...register('password', { required: true, pattern: /^\d+$/, minLength: 5 })}
           />
           {errors.password && (
-            <p>
+            <p className="text-red-500">
               {errors.password.type === 'required'
                 ? 'password is required'
                 : 'Invalid password pattern'}
@@ -105,26 +115,46 @@ export default function SignUp() {
           )}
         </div>
 
-        <input
-          type="file"
-          id="myfile"
-          {...register('myfile', { required: true, validate: validateFile })}
-        />
-        {errors.myfile &&
-          <p>
-            {errors.myfile.type === 'required'
-              ? 'file is required'
-              : 'invalid file'}
-          </p>
-        }
+        <div>
+          <label htmlFor="myfile" className="block font-medium">File</label>
+          <input
+            type="file"
+            id="myfile"
+            className="w-full"
+            {...register('myfile', { required: true, validate: validateFile })}
+          />
+          {errors.myfile && (
+            <p className="text-red-500">
+              {errors.myfile.type === 'required'
+                ? 'File is required'
+                : 'Invalid file'}
+            </p>
+          )}
+        </div>
+        <br>
+        </br>
+        <button
+          type="submit"
+          className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
 
-
-
-        <button type="submit">Submit</button>
+        >
+          Submit
+        </button>
       </form>
-      <button type="button" onClick={() => router.back()}>
-        Click here to go back
-      </button>
+
+      <div className="flex justify-center items-center mt-12">
+        <button
+          type="button"
+          onClick={() => router.push('/tender-manager/signin')}
+          className="mt-4 block underline hover:text-blue-500"
+        >
+          Already Have an Account? Click here to Login
+        </button>
+      </div>
+
+
+
     </>
   );
+
 }
