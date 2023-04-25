664737515119962a152b8f1a11b1bc8172386d89
jawadbs23 <66155771+mdjawadulhasan@users.noreply.github.com>
2023-04-26 00:02:50 +0600

    Update Done

--- pages/tender-manager/profile.js
+++ pages/tender-manager/profile.js
@@ -9,6 +9,8 @@ export default function GetUsers() {
   const router = useRouter();
 
   useEffect(() => {
+
+    
     const email = sessionStorage.getItem('email');
     if (!email) {
       router.push('/tender-manager/signin');
@@ -27,13 +29,31 @@ export default function GetUsers() {
     fetchData();
   }, [router]);
 
+  const handleUpdateClick = () => {
+
+    router.push(`/tender-manager/update`);
+
+  };
+
   return (
-    <div className="bg-gray-50 min-h-screen">
+    
+    <div className="bg-white-50 min-h-screen">
       <MyLayout title="Profile" />
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <h1 className="text-3xl font-bold text-gray-900 mb-4">My Profile</h1>
+        <div className="flex justify-end mb-4">
+
+        </div>
         {userData ? <UserLayout data={userData} /> : <p>Loading...</p>}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleUpdateClick}
+        >
+          Update
+        </button>
       </div>
+
+
     </div>
   );
 }
---
+++ pages/tender-manager/update.js
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+import MyLayout from "@/pages/tender-manager/component/layout";
+import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form';
+import axios from "axios"
+
+
+export default function UpdateProfile() {
+    const router = useRouter();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const email = sessionStorage.getItem('email');
+        if (!email) {
+            router.push('/tender-manager/signin');
+            return;
+        }
+
+        const fetchUser = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/TenderManager/viewprofilebyemail/${email}`);
+                setUser(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
+    const [success, setSuccess] = useState('')
+    const onSubmit = async (data) => {
+        const form = {
+            name: data.name,
+            email: data.email,
+            password: user.password,
+            ImgfileName: user.ImgfileName,
+            id: user.id,
+        };
+
+        try {
+            const response = await axios.put(`http://localhost:3000/TenderManager/update`, form);
+            router.push('/tender-manager/profile')
+            reset();
+        } catch (error) {
+            setSuccess('Update unsuccessful ' + error.response.data.message);
+        }
+    };
+
+
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+            <MyLayout title="Profile" />
+
+            <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm mx-auto mt-1">
+                <br></br>
+                <h1 className="text-2xl font-bold mb-4">Update Profile</h1>
+                <div>
+                    <label htmlFor="name" className="block font-medium">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        className="w-full border border-gray-400 p-2 rounded-md"
+                        {...register('name', { required: true })}
+                        defaultValue={user.name}
+                    />
+                    {errors.name && <p className="text-red-500">Name is required</p>}
+                </div>
+                <div>
+                    <label htmlFor="email" className="block font-medium">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="w-full border border-gray-400 p-2 rounded-md"
+                        {...register('email', { required: true, pattern: /\S+@\S+\.\S+/ })}
+                        defaultValue={user.email}
+                    />
+                    {errors.email && (
+                        <p className="text-red-500">
+                            {errors.email.type === 'required'
+                                ? 'Email is required'
+                                : 'Invalid email address'}
+                        </p>
+                    )}
+                </div>
+                <br></br>
+                <button
+                    type="submit"
+                    className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                    Update
+                </button>
+            </form>
+        </>
+    );
+}
