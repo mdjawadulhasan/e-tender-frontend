8ca920b55b30c78ec847a881ad13612b3bc7a27c
JoyMatubber <joy.matubber.41959@gmail.com>
2023-04-29 19:41:44 +0600

    Delete Feature Added

--- pages/Agency/profile.js
+++ pages/Agency/profile.js
@@ -3,12 +3,35 @@ import MyLayout from "@/pages/Agency/component/layout";
 import UserLayout from "./component/userdata";
 import router from "next/router";
 import SideLayout from "@/pages/Agency/component/sidebar";
+// import Swal from "sweetalert2";
 
 export default function GetUsers() {
   const userData = useAuth();
 
   const handleUpdateClick = () => {
-  router.push(`/Agency/update`);
+    router.push(`/Agency/update`);
+  };
+
+  const handleDeleteClick = () => {
+    fetch(`http://localhost:3000/Agency/delete/${userData.id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // if (data.statusCode === 500) {
+        // Swal.fire({
+        // icon: "error",
+        // title: "Oops...",
+        // text: "Cannot delete due to dependency issues!",
+        // footer: "",
+        // });
+        // } else {
+        // Swal.fire("Deleted!", "Sign in to Access !", "success");
+
+        router.push("/Agency/signin");
+        // }
+      })
+      .catch((error) => console.log(error)); // handle the error
   };
 
   return (
@@ -20,10 +43,17 @@ export default function GetUsers() {
         <div className="flex justify-end mb-4"></div>
         {userData ? <UserLayout data={userData} /> : <p>Loading...</p>}
         <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleUpdateClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleUpdateClick}
         >
-        Update
+          Update
+        </button>
+
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleDeleteClick}
+        >
+          Delete
         </button>
       </div>
     </div>
